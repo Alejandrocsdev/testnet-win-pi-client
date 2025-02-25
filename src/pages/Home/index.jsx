1167bcd46daede7d0e4eb4be316d67532464c92c
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-02-26 01:44:27 +0800

    test browser type

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,13 +3,22 @@ import S from './style.module.css'
 // 組件
 import CheckBrowser from '../../components/CheckBrowser'
 
+import { useEffect, useState } from 'react' // test browser
+
 // 首頁
 function Home() {
+  const [userAgent, setUserAgent] = useState('') // test browser
+
+  useEffect(() => { // test browser
+    setUserAgent(navigator.userAgent) // test browser
+  }, []) // test browser
+
   return (
     <main className={S.main}>
       <span>Home</span>
       <a href="/test">Go to Test</a>
       <CheckBrowser/>
+      <p>{userAgent}</p> // test browser
     </main>
   )
 }
