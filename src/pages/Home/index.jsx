1981cc650641592954369ef5c95503470b13282e
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-03-03 01:44:09 +0800

    test browser type (again) - update CheckBrowser component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,13 +3,32 @@ import S from './style.module.css'
 // 組件
 import CheckBrowser from '../../components/CheckBrowser'
 
+import { useState, useEffect } from 'react'
+
 // 首頁
 function Home() {
+  const [userAgent, setUserAgent] = useState('')
+  const [userAgentData, setUserAgentData] = useState(null)
+
+  useEffect(() => {
+    setUserAgent(navigator.userAgent)
+    setUserAgentData(navigator.userAgentData || null)
+  }, [])
+
   return (
     <main className={S.main}>
       <span>Home</span>
       <a href="/test">Go to Test</a>
       <CheckBrowser />
+      <div>
+        <h3>UserAgent:</h3>
+        <p>{userAgent}</p>
+      </div>
+
+      <div>
+        <h3>UserAgentData:</h3>
+        <pre>{JSON.stringify(userAgentData, null, 2)}</pre>
+      </div>
     </main>
   )
 }
