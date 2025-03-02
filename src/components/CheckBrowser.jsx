857e0d847b3a76698674eee259be96e6e6ba94db
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-03-02 22:22:57 +0800

    update checkBrowser to test new results - remove browser type testing (userAgent)

--- src/components/CheckBrowser.jsx
+++ src/components/CheckBrowser.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react'
+
 // 檢查瀏覽器
 function CheckBrowser() {
+  const [piBrowser, setPiBrowser] = useState(null)
+  const [miningApp, setMiningApp] = useState(null)
+  const [systemBrowser, setSystemBrowser] = useState(null)
   const handleBrowserCheck = async () => {
+    if (!window.Pi) {
+      console.error('Pi SDK is not available')
+      return
+    }
+
     try {
-      const result = await window.Pi.checkUserHasPiBrowser()
-      console.log('User has Pi Browser:', result)
+      const result1 = await window.Pi.checkUserHasPiBrowser()
+      const result2 = await window.Pi.checkUserHasMiningApp()
+      const result3 = await window.Pi.openUrlInSystemBrowser()
+
+      // Extracting correct values
+      setPiBrowser(result1)
+      setMiningApp(result2)
+      setSystemBrowser(result3)
+
+      console.log('User has Pi Browser:', result1)
+      console.log('User has Mining App:', result2)
+      console.log('User has Mining App:', result3)
     } catch (error) {
-      console.error(error)
+      console.error('Error checking browser or mining app:', error)
     }
   }
 
-  return <button onClick={handleBrowserCheck}>Check Browser</button>
+  return (
+    <>
+      <button onClick={handleBrowserCheck}>Check Browser</button>
+      <p>Pi Browser: {piBrowser !== null ? piBrowser.toString() : 'Checking...'}</p>
+      <p>Mining App: {miningApp !== null ? miningApp.toString() : 'Checking...'}</p>
+      <p>Mining App: {systemBrowser !== null ? systemBrowser.toString() : 'Checking...'}</p>
+    </>
+  )
 }
 
 export default CheckBrowser
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,13 @@ import S from './style.module.css'
 // 組件
 import CheckBrowser from '../../components/CheckBrowser'
 
-import { useEffect, useState } from 'react' // test browser
-
 // 首頁
 function Home() {
-  const [userAgent, setUserAgent] = useState('') // test browser
-
-  useEffect(() => { // test browser
-    setUserAgent(navigator.userAgent) // test browser
-  }, []) // test browser
-
   return (
     <main className={S.main}>
       <span>Home</span>
       <a href="/test">Go to Test</a>
-      <CheckBrowser/>
-      <p>{userAgent}</p> // test browser
+      <CheckBrowser />
     </main>
   )
 }
